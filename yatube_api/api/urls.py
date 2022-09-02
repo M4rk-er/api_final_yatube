9a2b4f829805d1fa03edc9adc8c897e87a9e2a5f
Марк Британов <britanovm@gmail.com>
2022-09-03 01:24:17 +0500

    Первая отправка

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,4 +1,20 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
+
+from .views import CommentViewSet, FollowViewSet, GroupViewSet, PostViewSet
+
+v1router = routers.DefaultRouter()
+v1router.register('posts', PostViewSet, basename='posts')
+v1router.register('groups', GroupViewSet, basename='groups')
+v1router.register(
+    r'posts/(?P<post_id>[\d]+)/comments',
+    CommentViewSet,
+    basename='comments'
+)
+v1router.register('follow', FollowViewSet, basename='follow')
 
 urlpatterns = [
+    path('v1/', include(v1router.urls)),
+    path('v1/', include('djoser.urls')),
+    path('v1/', include('djoser.urls.jwt')),
 ]
--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -1,4 +1,5 @@
 import os
+from datetime import timedelta
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -15,7 +16,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework.authtoken',
     'rest_framework',
+    'django_filters',
+    'djoser',
     'api',
     'posts',
 ]
@@ -90,4 +94,12 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=15),
+    'AUTH_HEADER_TYPES': ('Bearer',),
 }
